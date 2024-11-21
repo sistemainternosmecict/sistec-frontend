3dc1603bac65eea2b086578792991c9060f6d394
thyezoliveira <thyezoliveira@gmail.com>
2024-11-21 15:07:40 -0300

    ui(Usuarios): Os usuarios agora carregam no componente da area e a listagem fica livre da requisição.

--- src/components/areaDeUsuarios/ListagemUsuario/index.jsx
+++ src/components/areaDeUsuarios/ListagemUsuario/index.jsx
@@ -1,80 +1,28 @@
-import { useContext, useState, useEffect, useRef } from "react";
-import { HostContext } from "../../../HostContext";
+import { useEffect, useRef } from "react";
+import PropTypes from 'prop-types'
 import Usuario from "../Usuario";
 import icone from '../../../assets/icone_processamento.png';
 import './style.scss'
 
-async function obter_usuarios(host){
-    const route = "/api/usuarios/listar"
-    const result = await fetch(host+route);
-    const retorno = await result.json();
-    return retorno
-}
-
-function buscar( valor, lista, setParaRenderizar ){
-    const termoNormalizado = valor.trim()
-    const tipo = /^\d+$/.test(termoNormalizado) ?
-        termoNormalizado.length === 11 ? "cpf" : "matricula" : "nome";
-
-    // console.log(tipo)
-
-    const itensEncontrados = lista.filter(item => {
-        if( termoNormalizado !== ""){
-            if(tipo == 'cpf'){
-                const cpf_inserido = termoNormalizado.toString()
-                const cpf_comparado = item.usuario_cpf.toString()
-                return cpf_comparado == cpf_inserido
-            }
-    
-            if(tipo == 'matricula'){
-                return item.usuario_matricula == Number(termoNormalizado)
-            }
-    
-            if(tipo == 'nome'){
-                if(item.usuario_nome.toLowerCase().includes(termoNormalizado.toLowerCase())){
-                    return item
-                }
-                // return item.usuario_nome.toLowerCase().includes(termoNormalizado.toLowerCase())
-            }
-        }
-
-        return false;
-    })
-
-    console.log(itensEncontrados)
-
-    setParaRenderizar(itensEncontrados)
-}
-
-export default function ListagemUsuarios(){
-    const [usuarios, setUsuarios] = useState([])
-    const { hostUrl } = useContext(HostContext)
-    const [paraRenderizar, setParaRenderizar] = useState([])
-    const blocs = useRef(paraRenderizar)
+function ListagemUsuarios({ usuarios }){
+    const blocs = useRef([])
 
     useEffect(() => {
-        async function fetchData() {
-            const data = await obter_usuarios(hostUrl)
-            setUsuarios(data.usuarios || [])
-            setParaRenderizar(data.usuarios)
+        if(usuarios.length > 0){
+            const lista = usuarios.map((usuario, index) => (
+                <li key={index}>
+                    <Usuario usuario={usuario}/>
+                </li>
+            ))
+        
+            const listao = <ul id="usuarios">
+                {lista}
+            </ul>
+        
+            blocs.current = listao
+            console.log(blocs)
         }
-        fetchData()
-        }, [hostUrl])
-
-    useEffect(() => {
-        const lista = paraRenderizar.map((usuario, index) => (
-            <li key={index}>
-                <Usuario usuario={usuario}/>
-            </li>
-        ))
-
-        const listao = <ul id="usuarios">
-            {lista}
-        </ul>
-
-        blocs.current = listao
-
-    }, [paraRenderizar])
+    }, [usuarios])
 
     return (
         <>  
@@ -92,3 +40,9 @@ export default function ListagemUsuarios(){
         </>
     )
 }
+
+ListagemUsuarios.propTypes = {
+    usuarios: PropTypes.array
+}
+
+export default ListagemUsuarios;
--- src/components/areaDeUsuarios/index.jsx
+++ src/components/areaDeUsuarios/index.jsx
@@ -1,23 +1,41 @@
-import { useState } from 'react';
+import { useState, useContext, useEffect } from 'react';
 import ListagemUsuarios from './ListagemUsuario';
 import CadastroUsuario from './CadastroUsuario';
 import NiveisDeAcesso from './NiveisDeAcesso';
+import { HostContext } from '../../HostContext';
 import './style.scss'
 
-function carregarSecao( pg, tipoDeArea ) {
+async function obter_usuarios(host){
+    const route = "/api/usuarios/listar"
+    const result = await fetch(host+route);
+    const retorno = await result.json();
+    return retorno
+}
+
+function carregarSecao( pg, tipoDeArea, usuarios ) {
     switch(pg){
         case 1:
             return <CadastroUsuario tipoDeArea={tipoDeArea} />
         case 2:
             return <NiveisDeAcesso />
         default:
-            return <ListagemUsuarios />
+            return <ListagemUsuarios usuarios={usuarios} />
     }
 }
 
 function AreaDeUsuarios() {
     const tipoDeArea = "interna"
     const [pagina, setPagina] = useState(0)
+    const { hostUrl } = useContext(HostContext)
+    const [usuarios, setUsuarios] = useState([])
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = await obter_usuarios(hostUrl)
+            setUsuarios(data.usuarios || [])
+        }
+        fetchData()
+    }, [hostUrl])
     
     return (
         <section>
@@ -29,7 +47,7 @@ function AreaDeUsuarios() {
                 </div>
             </aside>
             <main>
-                {carregarSecao(pagina, tipoDeArea)}
+                {carregarSecao(pagina, tipoDeArea, usuarios)}
             </main>
         </section>
     )
